2add9955ce16db46c813c61fd73bd76c9577fbe5
Kittipos <ki11ip0.s.a.s@gmail.com>
2023-07-15 23:06:32 +0700

    move inside `dev/`

--- dev/test.py
+++ dev/test.py
@@ -1,5 +1,8 @@
+import sys
+sys.path.insert(1, '..') # '/Users/kittipos/Documents/Radio-local/rad-proj/PedDesign-Py'
+# print(sys.path)
 import peddesign as pd
-#print(sys.path)
+
 
 # Design CT
 pd.design_ct("whole_abd", "younger_child", 6, "delay", 65)
